8e90cad7248ee03817025b56bc01c148e3c6e27d
林童奕凡 <129384364+Floyed11@users.noreply.github.com>
2024-04-27 21:48:28 +0800

    change db_conn.py

--- be/model/db_conn.py
+++ be/model/db_conn.py
@@ -6,32 +6,25 @@ class DBConn:
         self.db = store.get_db_conn()
 
     def user_id_exist(self, user_id):
-        cursor = self.conn.execute(
-            "SELECT user_id FROM user WHERE user_id = ?;", (user_id,)
-        )
-        row = cursor.fetchone()
-        if row is None:
+        user_col = self.db['user']
+        doc = user_col.find_one({'user_id': user_id})
+        if doc is None:
             return False
         else:
             return True
 
     def book_id_exist(self, store_id, book_id):
-        cursor = self.conn.execute(
-            "SELECT book_id FROM store WHERE store_id = ? AND book_id = ?;",
-            (store_id, book_id),
-        )
-        row = cursor.fetchone()
-        if row is None:
+        store_col = self.db['store']
+        doc = store_col.find_one({'store_id': store_id, 'book_id': book_id})
+        if doc is None:
             return False
         else:
             return True
 
     def store_id_exist(self, store_id):
-        cursor = self.conn.execute(
-            "SELECT store_id FROM user_store WHERE store_id = ?;", (store_id,)
-        )
-        row = cursor.fetchone()
-        if row is None:
+        user_store_col = self.db['user_store']
+        doc = user_store_col.find_one({'store_id': store_id})
+        if doc is None:
             return False
         else:
             return True
