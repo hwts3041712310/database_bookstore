d6689a66920f604d40ffc6c6b1fb8a48757bf7ea
predormit <10225102494@stu.ecnu.edu.cn>
2024-04-27 22:48:09 +0800

    db_conn.py

--- be/model/db_conn.py
+++ be/model/db_conn.py
@@ -4,34 +4,30 @@ from be.model import store
 class DBConn:
     def __init__(self):
         self.conn = store.get_db_conn()
+        self.users_col = self.conn['user']
+        self.store_col = self.conn['store']
+        self.user_store_col = self.conn['user_store']
 
     def user_id_exist(self, user_id):
-        cursor = self.conn.execute(
-            "SELECT user_id FROM user WHERE user_id = ?;", (user_id,)
-        )
-        row = cursor.fetchone()
-        if row is None:
+        result = self.users_col.find({'user_id':user_id})
+
+        if result is None:
             return False
         else:
             return True
 
     def book_id_exist(self, store_id, book_id):
-        cursor = self.conn.execute(
-            "SELECT book_id FROM store WHERE store_id = ? AND book_id = ?;",
-            (store_id, book_id),
-        )
-        row = cursor.fetchone()
-        if row is None:
+        result = self.store_col.find({'store_id':store_id},{'book_id':book_id})
+
+        if result is None:
             return False
         else:
             return True
 
     def store_id_exist(self, store_id):
-        cursor = self.conn.execute(
-            "SELECT store_id FROM user_store WHERE store_id = ?;", (store_id,)
-        )
-        row = cursor.fetchone()
-        if row is None:
+        result = self.user_store_col.find({'store_id':store_id})
+
+        if result is None:
             return False
         else:
             return True
